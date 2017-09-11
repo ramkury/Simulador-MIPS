66aac9cbd0b24031be0d8f6950e1a5beada4aae3
Ricardo <ricardokury@outlook.com>
2017-09-10 23:45:20 -0300

    Adiciona funções lhu e lbu.

--- isa.cpp
+++ isa.cpp
@@ -1,7 +1,9 @@
 #include "isa.h"
 
 int32_t mem[MEM_SIZE] = { 0 };
-int32_t GPR[32] = { 0 };
+int32_t gpr[32] = { 0 };
+uint32_t pc = TEXT_START;
+uint32_t ri = 0;
 
 int32_t lw(uint32_t address, int16_t kte) {
     uint32_t addr = (address + kte) >> 2;
@@ -9,15 +11,23 @@ int32_t lw(uint32_t address, int16_t kte) {
 }
 
 int32_t lh(uint32_t address, int16_t kte) {
-    int16_t * m = (int16_t *)mem;
+    auto m = (int16_t *)mem;
     uint32_t addr = (address + kte) >> 1;
     return m[addr];
 }
 
+int32_t lhu(uint32_t address, int16_t kte) {
+    return 0xFFFF & lh(address, kte);
+}
+
 int32_t lb(uint32_t address, int16_t kte) {
-    int8_t * m = (int8_t *)mem;
+    auto m = (int8_t *)mem;
     uint32_t addr = (address + kte);
     return m[addr];
+}
+
+int32_t lbu(uint32_t address, int16_t kte) {
+    return 0xFF & lb(address, kte);
 }
 
 void sw(uint32_t address, int16_t kte, int32_t dado) {
@@ -26,14 +36,13 @@ void sw(uint32_t address, int16_t kte, int32_t dado) {
 }
 
 void sh(uint32_t address, int16_t kte, int16_t dado) {
-    int16_t * m = (int16_t *)mem;
+    auto m = (int16_t *)mem;
     uint32_t addr = (address + kte) >> 1;
     m[addr] = dado;
 }
 
 void sb(uint32_t address, int16_t kte, int8_t dado) {
-    int8_t * m = (int8_t *)mem;
+    auto m = (int8_t *)mem;
     uint32_t addr = (address + kte);
     m[addr] = dado;
 }
-
--- isa.h
+++ isa.h
@@ -2,19 +2,47 @@
 
 #include <cstdint>
 
-#define MEM_SIZE 4096
-#define DATA_START 0x00002000
-#define TEXT_START 0x00000000
+#define MEM_SIZE    4096
+#define DATA_START  0x00002000
+#define TEXT_START  0x00000000
+
+const char * reg_names[] = {
+        "zero", "at", "v0", "v1", "a0", "a1", "a2", "a3",
+        "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7",
+        "s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7",
+        "t8", "t9", "k0", "k1", "gp", "sp", "fp", "ra"
+};
 
 extern int32_t mem[MEM_SIZE];
-extern int32_t GPR[32];
+extern int32_t gpr[32];
+extern uint32_t pc, ri;
+
+enum OPCODES { // lembrem que so sao considerados os 6 primeiros bits dessas constantes
+    EXT=0x00,   LW=0x23,    LB=0x20,    LBU=0x24,
+    LH=0x21,    LHU=0x25,   LUI=0x0F,   SW=0x2B,
+    SB=0x28,    SH=0x29,    BEQ=0x04,   BNE=0x05,
+    BLEZ=0x06,  BGTZ=0x07,  ADDI=0x08,  SLTI=0x0A,
+    SLTIU=0x0B, ANDI=0x0C,  ORI=0x0D,   XORI=0x0E,
+    J=0x02,     JAL=0x03
+};
+
+enum FUNCT	{
+    ADD=0x20,   SUB=0x22,   MULT=0x18,  DIV=0x1A,
+    AND=0x24,   OR=0x25,    XOR=0x26,   NOR=0x27,
+    SLT=0x2A,   JR=0x08,    SLL=0x00,   SRL=0x02,
+    SRA=0x03,   SYSCALL=0x0c,   MFHI=0x10,	MFLO=0x12
+};
 
 // lê um inteiro alinhado - endereços múltiplos de 4
 int32_t lw(uint32_t address, int16_t kte);
 // lê meia palavra, 16 bits - retorna inteiro com sinal
 int32_t lh(uint32_t address, int16_t kte);
+// lê meia palavra, 16 bits - retorna inteiro sem sinal
+int32_t lhu(uint32_t address, int16_t kte);
 // lê um byte - retorna inteiro com sinal
 int32_t lb(uint32_t address, int16_t kte);
+// lê um byte - retorna inteiro sem sinal
+int32_t lbu(uint32_t address, int16_t kte);
 // escreve um inteiro alinhado na memória - endereços múltiplos de 4
 void sw(uint32_t address, int16_t kte, int32_t dado);
 // escreve meia palavra, 16 bits - endereços múltiplos de 2
